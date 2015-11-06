5cb8e42433a2c180bc9928fc695ad38baa78b3ce
Alexander Laarmann <alaarmann@gmx.net>
2015-11-06 23:22:17 +0100

    DRY up a little

--- spec/test.js
+++ spec/test.js
@@ -7,7 +7,7 @@
   regexp : true, undef : true,
   white  : true
 */
-/*globals require, describe, it, expect, jasmine */
+/*globals require, describe, it, expect, jasmine, beforeEach */
 
 var createObjectTreeProcessor = require('../main');
 
@@ -15,17 +15,23 @@ describe("ObjectTreeProcessor", function() {
   'use strict';
  
   describe("applyToEach", function() {
-    var someFunction = function () {};
-    var collection = {
-      'one' : 1,
-      'two' : someFunction,
-      'three' : 'A string value'
-    };
+    var someFunction; 
+    var collection;
+    var processor;
+    var processIt;
+
+    beforeEach(function() {
+      someFunction = function () {};
+      collection = {
+        'one' : 1,
+        'two' : someFunction,
+        'three' : 'A string value'
+      };
+      processor = createObjectTreeProcessor(collection);
+      processIt = jasmine.createSpy();
+    });
 
     it("applies function to each owned member", function() {
-      var processIt = jasmine.createSpy();
-      var processor = createObjectTreeProcessor(collection);
-
       processor.applyToEach(processIt);
       expect(processIt).toHaveBeenCalledWith('one', 1);
       expect(processIt).toHaveBeenCalledWith('two', someFunction);
@@ -34,44 +40,55 @@ describe("ObjectTreeProcessor", function() {
     });
 
     it("returns ObjectTreeProcessor object", function() {
-      var processIt = function () {};
-      var processor = createObjectTreeProcessor(collection);
-
       var result = processor.applyToEach(processIt);
       expect(result).toBe(processor);
     });
 
     describe("when object-tree is empty", function() {
+      beforeEach(function() {
+        collection = {};
+        processor = createObjectTreeProcessor(collection);
+      });
       it("does not apply function", function() {
-        var processIt = jasmine.createSpy();
-        var processor = createObjectTreeProcessor({});
-
         processor.applyToEach(processIt);
         expect(processIt).not.toHaveBeenCalled();
+      });
+      it("returns ObjectTreeProcessor object", function() {
+        var result = processor.applyToEach(processIt);
+        expect(result).toBe(processor);
       });
     });
 
     describe("when object-tree is null", function() {
+      beforeEach(function() {
+        collection = null;
+        processor = createObjectTreeProcessor(collection);
+      });
       it("does not apply function", function() {
-        var processIt = jasmine.createSpy();
-        var processor = createObjectTreeProcessor(null);
-
         processor.applyToEach(processIt);
         expect(processIt).not.toHaveBeenCalled();
+      });
+      it("returns ObjectTreeProcessor object", function() {
+        var result = processor.applyToEach(processIt);
+        expect(result).toBe(processor);
       });
     });
 
     describe("when this-pointer provided", function() {
-      it("applies function bound to this-pointer provided", function() {
-        var thisContextProvided = {a : 'R'};
-        var thisContextInFunction;
-        var processIt = function (key, value) {
+      var thisContextProvided;
+      var thisContextInFunction;
+
+      beforeEach(function() {
+        thisContextProvided = {a : 'R'};
+        thisContextInFunction = undefined;
+        processIt = function (key, value) {
 	  thisContextInFunction = this;
           this.a +=  value;
         };
-        var collection = {b : 'OMA'};
-        var processor = createObjectTreeProcessor(collection);
-
+        collection = {b : 'OMA'};
+        processor = createObjectTreeProcessor(collection);
+      });
+      it("applies function bound to this-pointer provided", function() {
         processor.applyToEach(processIt, thisContextProvided);
         expect(thisContextInFunction).toBe(thisContextProvided);
         expect(thisContextProvided.a).toEqual('ROMA');
@@ -79,14 +96,17 @@ describe("ObjectTreeProcessor", function() {
     });
 
     describe("when this-pointer not provided", function() {
-      it("applies function bound to null-context", function() {
-        var thisContextInFunction;
-        var processIt = function (key, value) {
+      var thisContextInFunction;
+
+      beforeEach(function() {
+        thisContextInFunction = undefined;
+        processIt = function (key, value) {
 	  thisContextInFunction = this;
         };
-        var collection = {b : 'OMA'};
-        var processor = createObjectTreeProcessor(collection);
-
+        collection = {b : 'OMA'};
+        processor = createObjectTreeProcessor(collection);
+      });
+      it("applies function bound to null-context", function() {
         processor.applyToEach(processIt);
         expect(thisContextInFunction).toBe(null);
       });
