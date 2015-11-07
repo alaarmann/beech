00b844adef863ab843b19c520bb626164bf3f5e0
Alexander Laarmann <alaarmann@gmx.net>
2015-11-07 23:20:58 +0100

    Use *this* for shared context

--- spec/test.js
+++ spec/test.js
@@ -15,62 +15,58 @@ describe("ObjectTreeProcessor", function() {
   'use strict';
  
   describe("applyToEach", function() {
-    var someFunction; 
-    var collection;
-    var processor;
-    var processIt;
 
     beforeEach(function() {
-      someFunction = function () {};
-      collection = {
+      this.someFunction = function () {};
+      this.collection = {
         'one' : 1,
-        'two' : someFunction,
+        'two' : this.someFunction,
         'three' : 'A string value'
       };
-      processor = createObjectTreeProcessor(collection);
-      processIt = jasmine.createSpy();
+      this.processor = createObjectTreeProcessor(this.collection);
+      this.processIt = jasmine.createSpy();
     });
 
     it("applies function to each owned member", function() {
-      processor.applyToEach(processIt);
-      expect(processIt).toHaveBeenCalledWith('one', 1);
-      expect(processIt).toHaveBeenCalledWith('two', someFunction);
-      expect(processIt).toHaveBeenCalledWith('three', 'A string value');
-      expect(processIt.calls.count()).toEqual(3);
+      this.processor.applyToEach(this.processIt);
+      expect(this.processIt).toHaveBeenCalledWith('one', 1);
+      expect(this.processIt).toHaveBeenCalledWith('two', this.someFunction);
+      expect(this.processIt).toHaveBeenCalledWith('three', 'A string value');
+      expect(this.processIt.calls.count()).toEqual(3);
     });
 
     it("returns ObjectTreeProcessor object", function() {
-      var result = processor.applyToEach(processIt);
-      expect(result).toBe(processor);
+      var result = this.processor.applyToEach(this.processIt);
+      expect(result).toBe(this.processor);
     });
 
     describe("when object-tree is empty", function() {
       beforeEach(function() {
-        collection = {};
-        processor = createObjectTreeProcessor(collection);
+        this.collection = {};
+        this.processor = createObjectTreeProcessor(this.collection);
       });
       it("does not apply function", function() {
-        processor.applyToEach(processIt);
-        expect(processIt).not.toHaveBeenCalled();
+        this.processor.applyToEach(this.processIt);
+        expect(this.processIt).not.toHaveBeenCalled();
       });
       it("returns ObjectTreeProcessor object", function() {
-        var result = processor.applyToEach(processIt);
-        expect(result).toBe(processor);
+        var result = this.processor.applyToEach(this.processIt);
+        expect(result).toBe(this.processor);
       });
     });
 
     describe("when object-tree is null", function() {
       beforeEach(function() {
-        collection = null;
-        processor = createObjectTreeProcessor(collection);
+        this.collection = null;
+        this.processor = createObjectTreeProcessor(this.collection);
       });
       it("does not apply function", function() {
-        processor.applyToEach(processIt);
-        expect(processIt).not.toHaveBeenCalled();
+        this.processor.applyToEach(this.processIt);
+        expect(this.processIt).not.toHaveBeenCalled();
       });
       it("returns ObjectTreeProcessor object", function() {
-        var result = processor.applyToEach(processIt);
-        expect(result).toBe(processor);
+        var result = this.processor.applyToEach(this.processIt);
+        expect(result).toBe(this.processor);
       });
     });
 
@@ -81,15 +77,15 @@ describe("ObjectTreeProcessor", function() {
       beforeEach(function() {
         thisContextProvided = {a : 'R'};
         thisContextInFunction = undefined;
-        processIt = function (key, value) {
+        this.processIt = function (key, value) {
 	  thisContextInFunction = this;
           this.a +=  value;
         };
-        collection = {b : 'OMA'};
-        processor = createObjectTreeProcessor(collection);
+        this.collection = {b : 'OMA'};
+        this.processor = createObjectTreeProcessor(this.collection);
       });
       it("applies function bound to this-pointer provided", function() {
-        processor.applyToEach(processIt, thisContextProvided);
+        this.processor.applyToEach(this.processIt, thisContextProvided);
         expect(thisContextInFunction).toBe(thisContextProvided);
         expect(thisContextProvided.a).toEqual('ROMA');
       });
@@ -100,14 +96,14 @@ describe("ObjectTreeProcessor", function() {
 
       beforeEach(function() {
         thisContextInFunction = undefined;
-        processIt = function (key, value) {
+        this.processIt = function (key, value) {
 	  thisContextInFunction = this;
         };
-        collection = {b : 'OMA'};
-        processor = createObjectTreeProcessor(collection);
+        this.collection = {b : 'OMA'};
+        this.processor = createObjectTreeProcessor(this.collection);
       });
       it("applies function bound to null-context", function() {
-        processor.applyToEach(processIt);
+        this.processor.applyToEach(this.processIt);
         expect(thisContextInFunction).toBe(null);
       });
     });
